27e77ac072cd5ee29d1a36de812c3cc60e879f14
EMMANUEL J ZAFRA <ejayzee@mac.com>
2017-01-07 21:45:12 -0800

    no message

---
+++ Remember This/MemoryCell.swift
@@ -0,0 +1,13 @@
+//
+//  MemoryCell.swift
+//  Remember This
+//
+//  Created by EMMANUEL J ZAFRA on 1/7/17.
+//  Copyright © 2017 EMMANUEL J ZAFRA. All rights reserved.
+//
+
+import UIKit
+
+class MemoryCell: UICollectionViewCell {
+    
+}
